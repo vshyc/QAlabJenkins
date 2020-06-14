d54e2d2b46058dd6ea8b17a94ea76edd04b990fb
ilusi0n <mateuszciolek@gmail.com>
2020-06-14 12:26:21 +0200

    Extended CreateUserTests.java

---
+++ src/main/java/pl/javastart/restassured/main/pojo/ApiResponse.java
@@ -0,0 +1,45 @@
+package pl.javastart.restassured.main.pojo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ApiResponse {
+
+    private Integer code;
+    private String type;
+    private String message;
+    private Map<String, Object> additionalProperties = new HashMap<>();
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
--- src/test/java/pl/javastart/restassured/tests/user/CreateUserTests.java
+++ src/test/java/pl/javastart/restassured/tests/user/CreateUserTests.java
@@ -1,10 +1,12 @@
 package pl.javastart.restassured.tests.user;
 
 import org.testng.annotations.Test;
+import pl.javastart.restassured.main.pojo.ApiResponse;
 import pl.javastart.restassured.main.pojo.user.User;
 import pl.javastart.restassured.tests.testbases.SuiteTestBase;
 
 import static io.restassured.RestAssured.given;
+import static org.testng.Assert.assertEquals;
 
 public class CreateUserTests extends SuiteTestBase {
 
@@ -20,10 +22,14 @@ public class CreateUserTests extends SuiteTestBase {
         user.setPhone("+123456789");
         user.setUserStatus(123);
 
-        given().contentType("application/json")
+        ApiResponse apiResponse = given().contentType("application/json")
                 .body(user)
                 .when().post("user")
-                .then().statusCode(200);
+                .then().statusCode(200).extract().as(ApiResponse.class);
+
+        assertEquals(apiResponse.getCode(), Integer.valueOf(200), "Code");
+        assertEquals(apiResponse.getType(), "unknown", "Type");
+        assertEquals(apiResponse.getMessage(), "445", "Message");
     }
 
 }
