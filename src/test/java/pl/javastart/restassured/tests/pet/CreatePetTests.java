ba895cac4430df68f8f0b785f7a104a4aa549bab
ilusi0n <mateuszciolek@gmail.com>
2020-06-27 10:02:51 +0200

    Added clean-up for CreatePetTests.java

--- src/test/java/pl/javastart/restassured/tests/pet/CreatePetTests.java
+++ src/test/java/pl/javastart/restassured/tests/pet/CreatePetTests.java
@@ -1,6 +1,8 @@
 package pl.javastart.restassured.tests.pet;
 
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
+import pl.javastart.restassured.main.pojo.ApiResponse;
 import pl.javastart.restassured.main.pojo.pet.Pet;
 import pl.javastart.restassured.main.test.data.pet.PetTestDataGenerator;
 import pl.javastart.restassured.tests.testbases.SuiteTestBase;
@@ -10,17 +12,30 @@ import static org.testng.Assert.assertEquals;
 
 public class CreatePetTests extends SuiteTestBase {
 
+    private Pet actualPet;
+
     @Test
     public void givenPetWhenPostPetThenPetIsCreatedTest() {
 
         Pet pet = new PetTestDataGenerator().generatePet();
 
-        Pet actualPet = given().body(pet).contentType("application/json")
+        actualPet = given().body(pet).contentType("application/json")
                 .when().post("pet")
                 .then().statusCode(200).extract().as(Pet.class);
 
         assertEquals(actualPet.getId(), pet.getId(), "Pet id");
         assertEquals(actualPet.getName(), pet.getName(), "Pet name");
+    }
+
+    @AfterMethod
+    public void cleanUpAfterTest(){
+        ApiResponse apiResponse = given().contentType("application/json")
+                .when().delete("pet/{petId}", actualPet.getId())
+                .then().statusCode(200).extract().as(ApiResponse.class);
+
+        assertEquals(apiResponse.getCode(), Integer.valueOf(200), "Code");
+        assertEquals(apiResponse.getType(), "unknown", "Type");
+        assertEquals(apiResponse.getMessage(), actualPet.getId().toString(), "Message"); // Sprawdzamy, czy w odpowiedzi systemu znajduje się usunięte petId zwierzątka
     }
 
 }
