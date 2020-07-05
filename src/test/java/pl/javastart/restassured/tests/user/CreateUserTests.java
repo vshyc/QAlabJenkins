df4e81e873d51180d251f98606f0ff98d16f8cb5
ilusi0n <mateuszciolek@gmail.com>
2020-07-05 10:34:51 +0200

    Added resolution for task F5 - CreateUserTests.java

--- src/test/java/pl/javastart/restassured/tests/user/CreateUserTests.java
+++ src/test/java/pl/javastart/restassured/tests/user/CreateUserTests.java
@@ -1,10 +1,12 @@
 package pl.javastart.restassured.tests.user;
 
+import org.apache.http.HttpStatus;
 import org.assertj.core.api.Assertions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 import pl.javastart.restassured.main.pojo.ApiResponse;
 import pl.javastart.restassured.main.pojo.user.User;
+import pl.javastart.restassured.main.request.configuration.RequestConfigurationBuilder;
 import pl.javastart.restassured.main.test.data.UserTestDataGenerator;
 import pl.javastart.restassured.tests.testbases.SuiteTestBase;
 
@@ -19,13 +21,13 @@ public class CreateUserTests extends SuiteTestBase {
         UserTestDataGenerator userTestDataGenerator = new UserTestDataGenerator();
         user = userTestDataGenerator.generateUser();
 
-        ApiResponse apiResponse = given().contentType("application/json")
+        ApiResponse apiResponse = given().spec(RequestConfigurationBuilder.getDefaultRequestSpecification())
                 .body(user)
                 .when().post("user")
-                .then().statusCode(200).extract().as(ApiResponse.class);
+                .then().statusCode(HttpStatus.SC_OK).extract().as(ApiResponse.class);
 
         ApiResponse expectedApiResponse = new ApiResponse();
-        expectedApiResponse.setCode(200);
+        expectedApiResponse.setCode(HttpStatus.SC_OK);
         expectedApiResponse.setType("unknown");
         expectedApiResponse.setMessage(user.getId().toString());
 
@@ -34,12 +36,12 @@ public class CreateUserTests extends SuiteTestBase {
 
     @AfterMethod
     public void cleanUpAfterTest() {
-        ApiResponse apiResponse = given().contentType("application/json")
+        ApiResponse apiResponse = given().spec(RequestConfigurationBuilder.getDefaultRequestSpecification())
                 .when().delete("user/{username}", user.getUsername())
-                .then().statusCode(200).extract().as(ApiResponse.class);
+                .then().statusCode(HttpStatus.SC_OK).extract().as(ApiResponse.class);
 
         ApiResponse expectedApiResponse = new ApiResponse();
-        expectedApiResponse.setCode(200);
+        expectedApiResponse.setCode(HttpStatus.SC_OK);
         expectedApiResponse.setType("unknown");
         expectedApiResponse.setMessage(user.getUsername());
 
