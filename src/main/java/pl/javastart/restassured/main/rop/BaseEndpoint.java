216facf8a3b7c06cf40b006a205c17076f8de7d3
ilusi0n <mateuszciolek@gmail.com>
2020-07-11 17:12:46 +0200

    Created BaseEndpoint and CreatePetEndpoint

---
+++ src/main/java/pl/javastart/restassured/main/rop/BaseEndpoint.java
@@ -0,0 +1,40 @@
+package pl.javastart.restassured.main.test.data;
+
+import io.restassured.response.Response;
+import org.assertj.core.api.Assertions;
+
+import java.lang.reflect.Type;
+
+//Parametr E - będzie oznaczał klasę Endpointu na przykład CreatePetEndpoint
+//Parametr M - będzie oznaczał klasę POJO (modelu) dla odpowiedzi
+public abstract class BaseEndpoint<E, M> {
+
+    // Pole do przechowywania odpowiedzi
+    protected Response response;
+
+    // Metoda do zwracania TYPU modelu, czyli klasy POJO. O tym czym jest Type dowiesz się później
+    protected abstract Type getModelType();
+
+    // Metoda do wysłania żądania, zwraca ona paramter E, czyli klasę endpointu
+    public abstract E sendRequest();
+
+    // Metoda do zwracania kodu sukcesu odpowiedzi
+    protected abstract int getSuccessStatusCode();
+
+    // Metoda zwraca odpowiedź w formacie modelu, zapis w metodzie jest równoznaczny z zapisem np. response.as(Pet.class);
+    public M getResponseModel() {
+        return response.as(getModelType());
+    }
+
+    //Metoda do weryfikacji czy kod statusowy jest poprawny tzw. sukces. Metoda bieżę wartość kodu statusowego z metody getSuccessStatusCode()
+    public E assertRequestSuccess() {
+        return assertStatusCode(getSuccessStatusCode());
+    }
+
+    // Metoda do weryfikacji koduy statusowego - dowolnego innego. Zwraca parametr (E), czyli klasę endpointu
+    public E assertStatusCode(int statusCode) {
+        Assertions.assertThat(response.getStatusCode()).as("Status code").isEqualTo(statusCode);
+        return (E) this;
+    }
+
+}
---
+++ src/main/java/pl/javastart/restassured/main/rop/CreatePetEndpoint.java
@@ -0,0 +1,37 @@
+package pl.javastart.restassured.main.rop;
+
+import org.apache.http.HttpStatus;
+import pl.javastart.restassured.main.pojo.pet.Pet;
+import pl.javastart.restassured.main.request.configuration.RequestConfigurationBuilder;
+import pl.javastart.restassured.main.test.data.BaseEndpoint;
+
+import java.lang.reflect.Type;
+
+import static io.restassured.RestAssured.given;
+
+public class CreatePetEndpoint extends BaseEndpoint<CreatePetEndpoint, Pet> {
+
+    private Pet pet;
+
+    @Override
+    protected Type getModelType() {
+        return Pet.class;
+    }
+
+    @Override
+    public CreatePetEndpoint sendRequest() {
+        response = given().spec(RequestConfigurationBuilder.getDefaultRequestSpecification()).body(pet)
+                .when().post("pet");
+        return this;
+    }
+
+    @Override
+    protected int getSuccessStatusCode() {
+        return HttpStatus.SC_OK;
+    }
+
+    public CreatePetEndpoint setPet(Pet pet) {
+        this.pet = pet;
+        return this;
+    }
+}
