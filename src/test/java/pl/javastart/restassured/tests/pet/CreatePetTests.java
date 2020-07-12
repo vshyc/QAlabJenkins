746810fbf6b5ea55581564c082c5f9850af40d0b
ilusi0n <mateuszciolek@gmail.com>
2020-07-12 11:07:59 +0200

    Full usage of ROP in CreatePetTests

--- src/test/java/pl/javastart/restassured/tests/pet/CreatePetTests.java
+++ src/test/java/pl/javastart/restassured/tests/pet/CreatePetTests.java
@@ -6,12 +6,10 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 import pl.javastart.restassured.main.pojo.ApiResponse;
 import pl.javastart.restassured.main.pojo.pet.Pet;
-import pl.javastart.restassured.main.request.configuration.RequestConfigurationBuilder;
 import pl.javastart.restassured.main.rop.CreatePetEndpoint;
+import pl.javastart.restassured.main.rop.DeletePetEndpoint;
 import pl.javastart.restassured.main.test.data.pet.PetTestDataGenerator;
 import pl.javastart.restassured.tests.testbases.SuiteTestBase;
-
-import static io.restassured.RestAssured.given;
 
 public class CreatePetTests extends SuiteTestBase {
 
@@ -28,10 +26,7 @@ public class CreatePetTests extends SuiteTestBase {
 
     @AfterMethod
     public void cleanUpAfterTest() {
-        ApiResponse apiResponse = given().spec(RequestConfigurationBuilder.getDefaultRequestSpecification())
-                .when().delete("pet/{petId}", actualPet.getId())
-                .then().statusCode(HttpStatus.SC_OK).extract().as(ApiResponse.class);
-
+        ApiResponse apiResponse = new DeletePetEndpoint().setPetId(actualPet.getId()).sendRequest().assertRequestSuccess().getResponseModel();
         ApiResponse expectedApiResponse = new ApiResponse();
         expectedApiResponse.setCode(HttpStatus.SC_OK);
         expectedApiResponse.setType("unknown");
